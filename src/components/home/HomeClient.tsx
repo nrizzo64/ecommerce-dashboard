ab1b20fd96b64b4bc1db7b2313657b3b586ac4ca
Teamups Dev <157994884+teamups-dev@users.noreply.github.com>
2025-01-31 08:20:38 -0600

    Update 'hello' message in home component

--- src/components/home/HomeClient.tsx
+++ src/components/home/HomeClient.tsx
@@ -115,7 +115,7 @@ export default function HomeClient(): React.ReactNode {
 
   return (
     <section>
-      <h1 style={{ fontSize: 24, marginBottom: 16 }}>Hello!</h1>
+      <h1 style={{ fontSize: 24, marginBottom: 16 }}>Hello, Nicholas!</h1>
       <Row>
         <Col span={12}>
           <Card title={<h3>Scout technical exercise</h3>} bordered={false}>
